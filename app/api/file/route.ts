9f5829742aa3ea5564942149aad6946ea0208141
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-08-15 00:20:25 +0300

    refactor: Add error handling for missing file in file upload API

--- app/api/file/route.ts
+++ app/api/file/route.ts
@@ -1,28 +1,31 @@
 import { NextResponse, NextRequest } from "next/server";
-import { pinata } from "@/utils/config"
+import { pinata } from "@/utils/config";
 
 export async function POST(request: NextRequest) {
   try {
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
-    const name:string=data.get("name");
-    const description:string=data.get("description");
-    const uploadImage = await pinata.upload.file(file)
-    const uploadData= await pinata.upload.json(
-        {
-            name:name,
-            description:description,
-            image:uploadImage.IpfsHash
-        }
-    )
+    const name: string = data.get("name");
+    const description: string = data.get("description");
+    if (!file || !name || !description) {
+      return NextResponse.json(
+        { error: "Please provide a file" },
+        { status: 400 }
+      );
+    }
+
+    const uploadImage = await pinata.upload.file(file);
+    const uploadData = await pinata.upload.json({
+      name: name,
+      description: description,
+      image: uploadImage.IpfsHash,
+    });
     // await axios.get(
     //     `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${uploadData.IpfsHash}`
     // ).then((res)=>{
     //     response=res.data
     // })
     return NextResponse.json(uploadData.IpfsHash, { status: 200 });
-    
-    
   } catch (e) {
     console.log(e);
     return NextResponse.json(
